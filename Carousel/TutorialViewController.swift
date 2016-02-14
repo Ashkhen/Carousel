3b62ad18834b2a2dcda882da271db043d1a264a5
Ashkhen Sargsyan <ashi@a5.io>
2016-02-14 12:53:41 -0800

    - Used scrollViewDidScroll instead of scrollViewDidEndDecelerating because it creates a more responsive look and feel.

--- Carousel/TutorialViewController.swift
+++ Carousel/TutorialViewController.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 
-class TutorialViewController: UIViewController {
+class TutorialViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var tutorialScrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var backupButton: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tutorialScrollView.contentSize = tutorialScrollView.frame.size
-        // Do any additional setup after loading the view.
+        tutorialScrollView.delegate = self
+        tutorialScrollView.contentSize = CGSize(width: 1280, height: 568)
+        backupButton.alpha = 0
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,25 +26,29 @@ class TutorialViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView!) {
+    func scrollViewDidScroll(scrollView: UIScrollView) {
         // Get the current page based on the scroll offset
         let page : Int = Int(round(tutorialScrollView.contentOffset.x / 320))
-        
+        pageControl.currentPage = page
         
         // Set the current page, so the dots will update
         pageControl.currentPage = page
+        if page == 3 {
+            pageControl.hidden = true
+            UIView.animateWithDuration(0.9, animations: {
+                self.backupButton.alpha = 1
+                }, completion: nil)
+            
+        } else {
+            UIView.animateWithDuration(0.3, animations: {
+                self.backupButton.alpha = 0
+                }, completion: nil)
+            pageControl.hidden = false
+        }
+        
     }
-    
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onTakeSpin(sender: AnyObject) {
+        performSegueWithIdentifier("toTimeLineNavSegue", sender: nil)
     }
-    */
-
 }
